64c31318a621d21ac4c5aadd31d2f03bd23a7520
Find Biao <997899594@qq.com>
2026-04-14 21:35:56 +0800

    chore: remove redundant db push wrapper

--- scripts/db-push.ts
+++
@@ -1,6 +0,0 @@
-import { applyControlPlaneMigrations } from '../src/lib/db/control-plane-atlas';
-
-applyControlPlaneMigrations().catch((error) => {
-  console.error(error instanceof Error ? error.message : String(error));
-  process.exit(1);
-});
